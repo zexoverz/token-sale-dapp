9ac5671750ac4aedbfeb3d6a3784cb57c824634c
faisal <faisalfirdani01@gmail.com>
2023-11-13 16:51:20 +0700

    Creating Constant Variable

---
+++ Context/constants.js
@@ -0,0 +1,15 @@
+// ZexoToken = 0x1DAdaF3bc9Ca303D71898D950bfea45A0d835d17
+// TokenSale = 0xdB67300e272150eF26523Ee30Efb4A9Cd98409C1
+
+// JSON FILE
+import ZexoToken from "./ZexoToken.json"
+import TokenSale from "./TokenSale.json"
+
+
+// TOKEN
+export const TOKEN_ADDRESS = "0x1DAdaF3bc9Ca303D71898D950bfea45A0d835d17";
+export const TOKEN_ABI = ZexoToken.abi
+
+// TOKEN SALE
+export const TOKEN_SALE_ADDRESS = "0xdB67300e272150eF26523Ee30Efb4A9Cd98409C1";
+export const TOKEN_SALE_ABI = TokenSale.abi
