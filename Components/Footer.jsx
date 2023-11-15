1dcdc12ab19aa8f409dc4c47b12030e40507ec1d
faisal <faisalfirdani01@gmail.com>
2023-11-15 15:14:24 +0700

    Finishing Footer Component

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,7 +1,92 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const footerList = [
+    "Zexocash", "How its work", "Token", "FAQ", "Contact"
+  ]
+  return <footer>
+    <div className="top_footer bg_light_dark"
+    data-z-index="1"
+    data-parallax="scroll"
+    data-image-src="assets/images/footer_bg.png">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4 col-md-12">
+            <div className="footer_logo mb-3 animation" data-animation="fadeInUp" data-animation-delay="0.2s">
+              <a href="#home_section" className="page-scroll">
+                <img src="assets/images/footer_logo.png" alt="" className="" />
+              </a>
+            </div>
+
+            <div className="footer_desc">
+              <p className="animation" data-animation="fadeInUp" data-animation-delay="0.4s">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis similique sed unde quod at nam adipisci non facilis, corrupti quaerat, fugit itaque iste error iusto dicta esse doloremque facere odit.
+              </p>
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-md-6 res_md_mt_30 res_sm_mt_20">
+            <h4 className="footer_title border_title animation" data-animation="fadeInUp" data-animation-delay="0.2s">
+              Quick Links
+            </h4>
+
+            <ul className="footer_link">
+              {
+                footerList.map((list, i) => (
+                  <li className="animation" data-animation="fadeInUp" data-animation-delay={`0.${i + 2}s`}>
+                    <a href="#">{list}</a>
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
+
+          <div className="col-lg-5 col-md-6 res_md_mt_30 res_sm_mt_20">
+            <div className="newsletter_form">
+              <h4 className="footer_title border_title animation">
+                Newsletter
+              </h4>
+
+              <p className="animation" data-animation="fadeInUp" data-animation-delay="0.4s">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nisi quam ut numquam asperiores distinctio mollitia. Quod exercitationem minus ipsa facere.
+              </p>
+
+              <form action="#" className="subscribe_form animation" data-animation="fadeInUp" data-animation-delay="0.4s">
+                <input type="text" required placeholder="Enter Email Address" className="input-rounded" />
+
+                <button type="submit" title="Subscribe" className="btn-info" name="submit" value="Submit">
+                  Subscribe
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div className="bottom_footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <p className="copyright">
+              Copyright &copy; 2023 All Right reserved by @zexoverz
+            </p>
+          </div>
+
+          <div className="col-md-6">
+            <ul className="list_none footer_menu">
+              <li>
+                <a href="#">Privacy Policy</a>
+              </li>
+              <li>
+                <a href="#">Terms & Conditions</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>;
 };
 
 export default Footer;
